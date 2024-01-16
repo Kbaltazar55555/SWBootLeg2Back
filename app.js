7546ebb84ee9f557fbc9d585127bc19dbdecb27b
Kbaltazar55555 <kenneth.baltazarv@gmail.com>
2024-01-16 05:58:02 -0500

    Added new

--- app.js
+++ app.js
@@ -60,3 +60,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+if (process.env.NODE_ENV !== 'production') {
+    require('dotenv').config();
+}
